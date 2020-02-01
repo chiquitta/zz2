00e24658f61c69ab6695c0332bc09d0def7d4a7c
Chiquitta Lin <chiquitta@brandma.co>
2020-02-01 23:14:32 +0800

    update publicPath

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@
 <script>
 export default {
   name: 'App',
-  // mounted() {
-  //   const { NODE_ENV } = process.env;
-  //   document.documentElement.className = NODE_ENV;
-  // },
+  mounted() {
+    const { NODE_ENV } = process.env;
+    document.documentElement.className = NODE_ENV;
+  },
 };
 </script>
 <style>
