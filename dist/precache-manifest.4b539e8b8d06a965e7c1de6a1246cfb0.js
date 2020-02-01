76db7d4e946992d55b60057d7c0b0712e713ff25
Chiquitta Lin <chiquitta@brandma.co>
2020-02-01 18:38:10 +0800

    create vue prj

---
+++ dist/precache-manifest.4b539e8b8d06a965e7c1de6a1246cfb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b41466c91a4c4c925f64",
+    "url": "/css/app.96126008.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9c1363af4a036317e476d53865172a7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d8846adb3cae3cecffd",
+    "url": "/js/about.2fcdeeed.js"
+  },
+  {
+    "revision": "b41466c91a4c4c925f64",
+    "url": "/js/app.f7bc73a7.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "/js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "4f47dcd25fd46f49f551f5733056ce72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
