8d6cc41264506dab3d66e48e86c6ea662afe6862
Chiquitta Lin <chiquitta@brandma.co>
2020-02-01 20:08:11 +0800

    update publicPath

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b69c746f6f7e40968bcac04bde5d8e36.js"
+  "/zz2/precache-manifest.f0b769682a0428579c91001c3b39ee43.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "zz2"});
