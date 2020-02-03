2814fb38b3c2c5def2596835981db6c6fede60ef
Chiquitta <linyuting@MacBook-Pro.local>
2020-02-03 11:02:03 +0800

    deploy

---
+++ precache-manifest.15d8394fc12c5351261aa6a4ad702fd7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca13c294eb83b259c0f",
+    "url": "/zz2/css/app.96126008.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/zz2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f46c13dad71b533afc842d0039b92648",
+    "url": "/zz2/index.html"
+  },
+  {
+    "revision": "1d8846adb3cae3cecffd",
+    "url": "/zz2/js/about.2fcdeeed.js"
+  },
+  {
+    "revision": "fca13c294eb83b259c0f",
+    "url": "/zz2/js/app.ce7e626c.js"
+  },
+  {
+    "revision": "6f93fb2e5f18372932d9",
+    "url": "/zz2/js/chunk-vendors.b42a6c66.js"
+  },
+  {
+    "revision": "4f47dcd25fd46f49f551f5733056ce72",
+    "url": "/zz2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/zz2/robots.txt"
+  }
+]);
